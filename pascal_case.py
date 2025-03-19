6726f24d72ce87926f41667581f0bac66224e0be
kenttyyy <kentajrins123@gmail.com>
2025-03-19 18:23:21 +0800

    proper casing

---
+++ pascal_case.py
@@ -0,0 +1,8 @@
+# Converts fullname into pascal case
+# Prompt to enter fullname using incorrect casing
+
+fullname = input("Please enter fullname in incorrect casing ")
+
+# Makes the proper casing first
+casing = fullname.title()
+
