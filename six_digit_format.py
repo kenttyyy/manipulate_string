bbb19f0fda6a7d4803281530d566ced93dd2f7bf
kenttyyy <kentajrins123@gmail.com>
2025-03-19 11:54:00 +0800

    check range and print

--- six_digit_format.py
+++ six_digit_format.py
@@ -1,5 +1,12 @@
 # Ask user to input numbers ranging from zero to a thousand, then add zero to complete a six digit format
-
 # User input prompt
 number = int(input("Please input a number "))
+
+# Check if a number ranges from zero to a thousand then add zero to make it 6 digit
+if 0 <= number <= 1000:
+    print(f"{number:06}")
+
+# If it is not within the range, say it is not
+else:
+    print("Please enter a value within the given range")
 
