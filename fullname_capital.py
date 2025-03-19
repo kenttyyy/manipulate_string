8d2146ff32934ba9f85532568a23a7105476342a
kenttyyy <kentajrins123@gmail.com>
2025-03-19 12:10:42 +0800

    changes to capital letter

---
+++ fullname_capital.py
@@ -0,0 +1,9 @@
+# Changes fullname to all capital letters
+# Ask user fullname
+fullname = input("Please enter your fullname ")
+
+# Make the full name capital letter
+capital = fullname.upper()
+
+# Then print it afterward
+print(capital)
