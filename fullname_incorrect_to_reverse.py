56dc42ec1e83b4054ff79d332131cf0c1869a709
kenttyyy <kentajrins123@gmail.com>
2025-03-19 17:24:24 +0800

    reverse the string

---
+++ fullname_incorrect_to_reverse.py
@@ -0,0 +1,9 @@
+# Changes incorrect casing and reverses it to what is the input
+# Prompts user to input
+fullname = input("Please enter a name using incorrect casing ")
+
+# Reverses the casing
+reverse = fullname.swapcase()
+
+# Prints the result
+print(reverse)
