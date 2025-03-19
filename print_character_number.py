ab8a0af5db77b8f8298ab7e108024a142342e5e3
kenttyyy <kentajrins123@gmail.com>
2025-03-19 17:54:50 +0800

    finds length then print

---
+++ print_character_number.py
@@ -0,0 +1,9 @@
+# Finds how many character are there in the user's fullname
+# Prompts user to input fullname
+fullname = input("Please enter your fullname ")
+
+# Counts how many characters are there
+character = len(fullname)
+
+# Prints how many characters are there
+print(character)
