b00fb0fcda7cfc5f876e93b23497345a10890dca
kenttyyy <kentajrins123@gmail.com>
2025-03-19 17:11:44 +0800

    title command print

---
+++ fullname_incorrect_to_proper.py
@@ -0,0 +1,9 @@
+# Changes incorrect casing into a proper casing
+# Asks for the user input
+fullname = input("Please input your fullname in incorrect casing ")
+
+# Changes the incorrect casing to proper
+proper = fullname.title()
+
+# Then prints it
+print(proper)
