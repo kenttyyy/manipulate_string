8c86efc8dcaeb224f88bf9964331e7c482a0e80a
kenttyyy <kentajrins123@gmail.com>
2025-03-19 17:45:28 +0800

    split the word then count

---
+++ number_of_words.py
@@ -0,0 +1,9 @@
+# Checks how many words in a statement
+# Prompts the user to input a statement
+statement = input("Please enter a statement ")
+
+# Counts how many words in a statement
+words = len(statement.split())
+
+# Prints the result
+print(words)
