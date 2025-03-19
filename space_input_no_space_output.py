6e86bb1f1fd40167fa7a9e8f2e4f003f53902512
kenttyyy <kentajrins123@gmail.com>
2025-03-19 11:09:58 +0800

    add spaces in input

---
+++ space_input_no_space_output.py
@@ -0,0 +1,4 @@
+# This program shows the input with spaces in the beginning and no spaces in output when printed
+
+# Ask the user to input number, and use tab to do some spaces in the input
+name = input("Please input your fullname\t\t")
